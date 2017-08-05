03fae7b9be6c5df0efe47907a234677e32ed51d3
Vermarohit1151 <30729211+Vermarohit1151@users.noreply.github.com>
2017-08-05 20:41:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/web', function (req, res) {
+   res.sendFile(path.join(_dirname,'ui','web.html')); 
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });

78b04b6c0ee5f76e408088a91d43f9a8ba8e65c3
Vermarohit1151 <30729211+Vermarohit1151@users.noreply.github.com>
2017-08-26 14:55:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,10 +5,10 @@ alert("I'm your master clicl OK to confirm.");
 var img=document.getElementById('madi');
 var marginLeft= 0;
 function moveRight(){
-    marginLeft= marginLeft + 5;
+    marginLeft= marginLeft + 1;
     img.style.marginLeft= marginLeft + 'px';
 }
 
  img.onclick = function() {
-     var interval = setInterval(moveRight, 10);
+     var interval = setInterval(moveRight, 50);
  };

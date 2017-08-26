2c2b4d68307df3235025145329b168570bd8f377
Vermarohit1151 <30729211+Vermarohit1151@users.noreply.github.com>
2017-08-26 14:43:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
-console.log('Loaded!');
+console.log('Loaded now!');
+//get
+alert("I'm your master clicl OK to confirm.");
+//ser
+var img=document.getElementById('madi');
+ img.onclick = function() {
+   img.style.marginLeft= '100px';  
+ };

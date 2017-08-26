ceddb7eceb7946d31f6dbd1ed252a11fafd71168
Vermarohit1151 <30729211+Vermarohit1151@users.noreply.github.com>
2017-08-26 14:51:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,12 @@ console.log('Loaded now!');
 alert("I'm your master clicl OK to confirm.");
 //ser
 var img=document.getElementById('madi');
+var marginLeft= 0;
+function moveRight(){
+    marginLeft= marginLeft+5;
+    img.style.marginLeft= marginLeft+'px';
+}
  img.onclick = function() {
-   img.style.marginLeft= '100px';  
+     var interval = setInterval(moveLRight, 100);
+   
  };

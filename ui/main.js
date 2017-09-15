ac089f4ad49cb58c0724060eb2c3b28486858160
Rohit Verma <30729211+Vermarohit1151@users.noreply.github.com>
2017-09-15 20:29:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded now!');
 //get
-alert("I'm your master clicl OK to confirm.");
+alert("I'm your master click OK to confirm.");
 //ser
 var img=document.getElementById('madi');
 var marginLeft= 0;
@@ -8,6 +8,8 @@ function moveRight(){
     marginLeft= marginLeft + 1;
     img.style.marginLeft= marginLeft + 'px';
 }
+
+
 
  img.onclick = function() {
      var interval = setInterval(moveRight, 5);
